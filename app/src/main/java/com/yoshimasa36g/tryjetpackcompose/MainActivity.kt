a67c89a315c13b49efe47336fc5abeac5a4686d7
yoshimasa36g <ashihana785@gmail.com>
2019-11-14 17:45:57 +0900

    Lesson 3, Material design

--- app/src/main/java/com/yoshimasa36g/tryjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/yoshimasa36g/tryjetpackcompose/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.compose.unaryPlus
+import androidx.ui.core.Clip
 import androidx.ui.core.Text
 import androidx.ui.core.dp
 import androidx.ui.core.setContent
 import androidx.ui.foundation.DrawImage
+import androidx.ui.foundation.shape.corner.RoundedCornerShape
 import androidx.ui.graphics.imageFromResource
 import androidx.ui.layout.*
 import androidx.ui.material.MaterialTheme
+import androidx.ui.material.themeTextStyle
+import androidx.ui.material.withOpacity
 import androidx.ui.res.imageResource
+import androidx.ui.text.style.TextOverflow
 import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
@@ -28,19 +33,33 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun NewStory() {
     val image = +imageResource(R.drawable.header)
-    Column(
+
+    MaterialTheme {
+        Column(
             crossAxisSize = LayoutSize.Expand,
             modifier = Spacing(16.dp)
-    ) {
-        Container(expanded = true, height = 180.dp) {
-            DrawImage(image)
+        ) {
+            Container(expanded = true, height = 180.dp) {
+                Clip(shape = RoundedCornerShape(8.dp)) {
+                    DrawImage(image)
+                }
+            }
+
+            HeightSpacer(height = 16.dp)
+
+            Text("A day wandering through the sandhills in Shark " +
+                "Fin Cove, and a few of the sights I saw",
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                style = (+themeTextStyle { h6 })
+                    .withOpacity(0.87f))
+            Text("Davenport, California",
+                style = (+themeTextStyle { body2 })
+                    .withOpacity(0.87f))
+            Text("December 2018",
+                style = (+themeTextStyle { body2 })
+                    .withOpacity(0.6f))
         }
-
-        HeightSpacer(height = 16.dp)
-
-        Text("A day in Shark Fin Cove")
-        Text("Davenport, California")
-        Text("December 2018")
     }
 }
 
