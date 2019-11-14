cea4e07167c2f0fa3fa07395ee47d20dc4f146ec
yoshimasa36g <ashihana785@gmail.com>
2019-11-14 17:32:33 +0900

    Lesson 2, Layout

--- app/src/main/java/com/yoshimasa36g/tryjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/yoshimasa36g/tryjetpackcompose/MainActivity.kt
@@ -3,9 +3,15 @@ package com.yoshimasa36g.tryjetpackcompose
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.compose.unaryPlus
 import androidx.ui.core.Text
+import androidx.ui.core.dp
 import androidx.ui.core.setContent
+import androidx.ui.foundation.DrawImage
+import androidx.ui.graphics.imageFromResource
+import androidx.ui.layout.*
 import androidx.ui.material.MaterialTheme
+import androidx.ui.res.imageResource
 import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
@@ -13,21 +19,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                Greeting("Android")
+                NewStory()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun NewStory() {
+    val image = +imageResource(R.drawable.header)
+    Column(
+            crossAxisSize = LayoutSize.Expand,
+            modifier = Spacing(16.dp)
+    ) {
+        Container(expanded = true, height = 180.dp) {
+            DrawImage(image)
+        }
+
+        HeightSpacer(height = 16.dp)
+
+        Text("A day in Shark Fin Cove")
+        Text("Davenport, California")
+        Text("December 2018")
+    }
 }
 
 @Preview
 @Composable
 fun DefaultPreview() {
-    MaterialTheme {
-        Greeting("Android")
-    }
+    NewStory()
 }
